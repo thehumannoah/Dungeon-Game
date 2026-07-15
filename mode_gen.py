1ef2fa6d4428c2ee4a48336bca001fd9e8c702b9
Noah Adams <noah.w.adams26@gmail.com>
2026-07-15 23:37:02 +0000

    Transferring to new laptop

--- mode_gen.py
+++ mode_gen.py
@@ -3,26 +3,32 @@ import random as Rand
 SECTION_LENGTH = 25 # Must be odd
 CENT = SECTION_LENGTH // 2
 
+def room_gen(tilemap, y_loc, x_loc, y_len, x_len):
+    for i in range(y_loc, y_loc + y_len):
+        for j in range(x_loc, x_loc + x_len):
+            tilemap[i][j] = 1
+    
+    tilemap[y_loc][x_loc] = 2
 
-
-def start(tilemap):
+def start(tilemap, entrances):
     # Middle square
     with open("section_templates/start.txt", "r") as f:
         file_text = f.read().split()
         for i in range(len(file_text)):
             tilemap[i] = [int(c) for c in file_text[i]]
     
-    return {'N': CENT, 'E': CENT, 'W': CENT}
+    entrances.update({'N': CENT, 'E': CENT, 'W': CENT})
 
 def corridor(tilemap, entrances):
-    hub_y = CENT #Rand.randint(CENT - 4, CENT + 4)
     hub_x = CENT #Rand.randint(CENT - 4, CENT + 4)
-    
+    hub_y = CENT #Rand.randint(CENT - 4, CENT + 4)
+
     tilemap[hub_y][hub_x] = 2
     
     for i in entrances:
         if entrances[i] != False:
-            if entrances[i] == None and Rand.random() <= .5: entrances[i] = Rand.randint(3, SECTION_LENGTH - 4)
+            chance = Rand.random()
+            if entrances[i] == None and chance <= .5: entrances[i] = Rand.randint(3, SECTION_LENGTH - 4)
             
             if entrances[i] != None:
                 match i:
@@ -34,3 +40,4 @@ def corridor(tilemap, entrances):
                         tilemap[SECTION_LENGTH - 1][entrances[i]] = 1
                     case 'W':
                         tilemap[entrances[i]][0] = 1
+    
--- section_class.py
+++ section_class.py
@@ -26,22 +26,29 @@ VALID_MODES = {
 
 class Section():
     def __init__(self, mode = "EMPTY", entrances = None):
-        
+        """
+        Sets Section variables and creates tilemap.
+        Holds all info on a section.
+        """
+        # Check if mode is valid
         if mode not in VALID_MODES:
             raise ValueError(f"Unknown section mode: {mode}")
         
+        # Set self variables
         self.mode = mode
         self.tilemap = [[0] * SECTION_LENGTH for _ in range(SECTION_LENGTH)]
         
+        # Set entrances to none and update with passed entrances (False means no entrance, None means possible entrance)
         self.entrances = {'N': None, 'E': None, 'S': None, 'W': None}
         if entrances:
             self.entrances.update(entrances)
         
+        # Execute corresponding function for each mode
         match self.mode:
             case "EMPTY":
                 pass
             case "START":
-                self.entrances.update(mg.start(self.tilemap))
+                mg.start(self.tilemap, self.entrances)
             case "CORRIDOR":
                 mg.corridor(self.tilemap, self.entrances)
             case "INTERSECTION":
@@ -54,12 +61,26 @@ class Section():
                 pass
     
     def print_grid(self):
+        """
+        Display the section's tilemap in the terminal.
+        Used for debugging the dungeon generator.
+        """
         RED = "\033[31m"
         BLUE = "\033[34m"
         GREEN = "\033[32m"
         RESET = "\033[0m"
+        
+        print("    ", end = "")
+        for i in range(SECTION_LENGTH):
+            if i <= 8: print(i, end = "  ")
+            else: print(i, end = " ")
+        print()
 
+        i = 0
         for row in self.tilemap:
+            if i <= 9: print(f" {i}", end = "  ")
+            else: print(i, end = "  ")
+            i += 1
             for cell in row:
                 if cell == 1:
                     print(f"{BLUE}{cell}{RESET}", end="  ")
@@ -70,6 +91,25 @@ class Section():
             print()
 
 if __name__ == "__main__":
-    test = Section(mode = "CORRIDOR", entrances = {'N': False, 'E': None, 'S': None, 'W': None})
+    
+    print("Select a mode:"
+        "\n\t1. room_gen"
+        "\n\t2. start"
+        "\n\t3. corridor")
+    choice = input()
+    print()
+
+    match choice:
+        case "1":
+            test = Section("EMPTY")
+            mg.room_gen(test.tilemap, 2, 2, 3, 3)
+        case "2":
+            test = Section("START")
+        case "3":
+            test = Section("CORRIDOR", {'N': CENT, 'E': False, 'S': CENT, 'W': False})
+        case _:
+            print("command not found.")
+            exit()
+    
     test.print_grid()
-    print(test.entrances)
+    print("\n", test.entrances, "\n")
